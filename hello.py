e428ffcc4c7da135ecee692b7d8969d77d3e09be
pyoewono <pyoewono@gmail.com>
2021-06-01 22:17:30 +0700

    hello 1

---
+++ hello.py
@@ -0,0 +1,10 @@
+# This program says helo and asks for my name
+print('Hello world')
+print('What is your name')
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The Lenght of your name is: ')
+print(len(myName))
+print('what is your age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) +' in a year')
